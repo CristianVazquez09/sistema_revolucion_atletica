8aa3742cece8c83490fe3127acbbbb1f98ffadec
Cristian <CristianVazquez09@users.noreply.github.com>
2025-08-06 17:49:27 -0600

    refactor: eliminación del módulo de Inscripción (entidad, DTO, capa service y controller)
    - Se eliminaron clases relacionadas con la entidad Inscripción:
      - Modelo: Inscripcion.java
      - DTO: InscripcionDTO.java
      - Controller: InscripcionController.java
      - Repositorio: IInscripcionRepo.java
      - Servicio: IInscripcionService.java e implementación
    - Esta decisión se tomó para simplificar el dominio actual del sistema, dejando la lógica de inscripción integrada en otras entidades o para redefinirla más adelante si es necesario

--- src/main/java/com/wolfpack/repo/IInscripcionRepo.java
+++
@@ -1,6 +0,0 @@
-package com.wolfpack.repo;
-
-import com.wolfpack.model.Inscripcion;
-
-public interface IInscripcionRepo extends IGenericRepo<Inscripcion, Integer>{
-}
--- src/main/java/com/wolfpack/service/IInscripcionService.java
+++
@@ -1,8 +0,0 @@
-package com.wolfpack.service;
-
-import com.wolfpack.model.Inscripcion;
-
-
-public interface IInscripcionService extends ICRUD<Inscripcion, Integer>{
-
-}
